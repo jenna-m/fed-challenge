cf38a26b5749b79f35473aa4c2b4fa15aeae4f81
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-05-31 14:07:01 -0400

    Update App.js
    Import page section components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './App.scss';
 
+// Import page sections
+import Navigation from './sections/navigation';
+import Hero from './sections/hero';
+import Reviews from './sections/reviews';
+import Workouts from './sections/workouts';
+import Equipment from './sections/equipment';
+import Footer from './sections/footer';
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div>
+			<Navigation />
+			<Hero />
+			<Reviews />
+			<Workouts />
+			<Equipment />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
