d52bfa17a6acd5fbadd9a5209464880e92b64ab8
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-05-31 15:33:02 -0400

    Start building navigation component and styling

--- src/sections/navigation.js
+++ src/sections/navigation.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import iFitLogo from '../images/svg-logos/ifit-coach-logo.svg';
 
 function Navigation() {
-	return <div>Navigation</div>;
+	return (
+		<div id="navigation">
+			<nav id="top-nav">
+				<ul id="top-nav-ul">
+					<li>Blog</li>
+					<li>Nourish</li>
+					<li>Shop</li>
+				</ul>
+			</nav>
+
+			<nav id="sub-nav">
+				<div id="logo">
+					<img src={iFitLogo} alt="iFit Logo" />
+				</div>
+				<div id="sub-nav-links">
+					<ul id="sub-nav-ul">
+						<li>Exercise</li>
+						<li>Nutrition</li>
+						<li>Activity</li>
+						<li>Sleep</li>
+					</ul>
+				</div>
+				<div id="sign-up-button">
+					<button>Sign Up</button>
+				</div>
+			</nav>
+		</div>
+	);
 }
 
 export default Navigation;
