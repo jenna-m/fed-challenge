60a42297bc08ee703ac401cfad185bddcca86a33
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-05-31 17:59:54 -0400

    Add hero section and styling

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function Hero() {
-	return <div>Hero</div>;
+	return (
+		<div id="hero">
+			<div id="hero-message">
+				The best personal training, right in your own home
+				<div id="hero-button">
+					<button>Join iFit Coach</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Hero;
