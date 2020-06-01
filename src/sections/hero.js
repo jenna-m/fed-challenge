094d7e7aaa82ed15baeb23733a1ba225f5a6c990
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-06-01 17:16:37 -0400

    Update hero.js
    Wrap hero message in span

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -4,7 +4,7 @@ function Hero() {
 	return (
 		<div id="hero">
 			<div id="hero-message">
-				The best personal training, right in your own home
+				<span>The best personal training, right in your own home</span>
 				<div id="hero-button">
 					<button>Join iFit Coach</button>
 				</div>
