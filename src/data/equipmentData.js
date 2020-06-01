ba75893454917fdd59805bcc1997fc73ec50c529
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-06-01 13:29:33 -0400

    Create equipmentData.js
    Create array fro equipment data

---
+++ src/data/equipmentData.js
@@ -0,0 +1,24 @@
+// Import equipment photos
+import treadmills from '../images/equipment/treadmills.png';
+import bikes from '../images/equipment/bikes.png';
+import ellipticals from '../images/equipment/ellipticals.png';
+import strength from '../images/equipment/strength.png';
+
+const equipmentData = [
+	{
+		title: 'Treadmills',
+		src: treadmills,
+	},
+	{
+		title: 'Bikes',
+		src: bikes,
+	},
+	{
+		title: 'Ellipticals',
+		src: ellipticals,
+	},
+	{
+		title: 'Strength',
+		src: strength,
+	},
+];
