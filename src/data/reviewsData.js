a670a4be6eeb2afc5a48058c8257c2bed572f33b
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-06-01 13:22:24 -0400

    Create reviewsData.js
    Create array for reviews data

---
+++ src/data/reviewsData.js
@@ -0,0 +1,26 @@
+// Import reviewer logos
+import gearJunkieLogo from '../images/svg-logos/gear-junkie-logo.svg';
+import wiredLogo from '../images/svg-logos/wired-logo.svg';
+import mashableLogo from '../images/svg-logos/mashable-logo.svg';
+
+const reviewsData = [
+	{
+		logoSRC: gearJunkieLogo,
+		alt: 'Gear Junkie Logo',
+		content:
+			'You focus on putting in the work, and the technology handles the rest.',
+	},
+	{
+		logoSRC: wiredLogo,
+		alt: 'Wired Logo',
+		content:
+			'Literally transports you from home to wherever you choose to run.',
+	},
+	{
+		logoSRC: mashableLogo,
+		alt: 'Mashable Logo',
+		content: 'Breathes new life into a tired, old running routine.',
+	},
+];
+
+export default reviewsData;
