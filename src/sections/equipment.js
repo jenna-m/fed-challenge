5e0284d22a6d3911eb103ccf80b9bd735cbc93a1
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-06-01 13:44:52 -0400

    Update equipment.js
    Map equipment info from equipmentData.js

--- src/sections/equipment.js
+++ src/sections/equipment.js
@@ -1,7 +1,27 @@
 import React from 'react';
+// Import equipment data
+import equipmentData from '../data/equipmentData';
 
 function Equipment() {
-	return <div>Equipment</div>;
+	// map all equipment data
+	const equipment = equipmentData.map((equipment) => {
+		return (
+			<div className="equipment-card">
+				<div className="equipment-image">
+					<img src={equipment.src} alt={equipment.alt} />
+				</div>
+				<div className="equipment-text">
+					<p>{equipment.title}</p>
+				</div>
+			</div>
+		);
+	});
+	return (
+		<div id="equipment-container">
+			<h3>Interested in our exciting iFit-enabled equipment?</h3>
+			<div id="equipment-cards-container">{equipment}</div>
+		</div>
+	);
 }
 
 export default Equipment;
