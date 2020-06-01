10872fb92b4f81c46348d22ceb22e4f4d9654479
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-06-01 17:16:22 -0400

    Update footer.js
    Start building footer

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div id="footer-container">
+			<div id="footer-menu">
+				<div className="footer-submenu">
+					<ul aria-label="Company">
+						<li>About</li>
+						<li>Contact Us</li>
+						<li>Careers</li>
+					</ul>
+				</div>
+				<div className="footer-submenu">
+					<ul aria-label="Account">
+						<li>Log In</li>
+						<li>Create Account</li>
+					</ul>
+				</div>
+				<div className="footer-submenu">
+					<ul aria-label="Support">
+						<li>Help Center</li>
+						<li>Accessibility</li>
+					</ul>
+				</div>
+			</div>
+			<div id="footer-social">Social Media Links</div>
+			<div id="footer-legal">Footer Legal Info</div>
+		</div>
+	);
 }
 
 export default Footer;
