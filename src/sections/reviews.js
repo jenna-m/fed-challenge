cb5f2aeb900a419fd5e450418d6eca1e00382d1e
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-06-01 13:23:05 -0400

    Refator reviews.js
    Refactor hard coded reviews to be mapped from reviewsData.js

--- src/sections/reviews.js
+++ src/sections/reviews.js
@@ -1,45 +1,22 @@
 import React from 'react';
-import gearJunkieLogo from '../images/svg-logos/gear-junkie-logo.svg';
-import wiredLogo from '../images/svg-logos/wired-logo.svg';
-import mashableLogo from '../images/svg-logos/mashable-logo.svg';
+// Import reviews data
+import reviewsData from '../data/reviewsData';
 
 function Reviews() {
-	return (
-		<div id="reviews">
+	// map all review data
+	const reviews = reviewsData.map((review) => {
+		return (
 			<div className="review-card">
 				<div className="review-source">
-					<img src={gearJunkieLogo} alt="Gear Junkie Logo" />
+					<img src={review.logoSRC} alt={review.alt} />
 				</div>
 				<div className="review-text">
-					<p>
-						“You focus on putting in the work, and the technology
-						handles the rest.”
-					</p>
+					<p>“{review.content}”</p>
 				</div>
 			</div>
-			<div className="review-card">
-				<div className="review-source">
-					<img src={wiredLogo} alt="Wired Logo" />
-				</div>
-				<div className="review-text">
-					<p>
-						“Literally transports you from home to wherever you
-						choose to run.”
-					</p>
-				</div>
-			</div>
-			<div className="review-card">
-				<div className="review-source">
-					<img src={mashableLogo} alt="Mashable Logo" />
-				</div>
-				<div className="review-text">
-					<p>
-						“Breathes new life into a tired, old running routine.”
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+		);
+	});
+	return <div id="reviews">{reviews}</div>;
 }
 
 export default Reviews;
