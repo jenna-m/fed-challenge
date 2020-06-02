b329f55dfa73ba23e99d8c89c38bbb786ed2310b
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-06-02 09:44:51 -0400

    Update navigation
    Hide navigation once past hero section

--- src/sections/navigation.js
+++ src/sections/navigation.js
@@ -1,35 +1,75 @@
 import React from 'react';
+import classnames from 'classnames';
 import iFitLogo from '../images/svg-logos/ifit-coach-logo.svg';
 
-function Navigation() {
-	return (
-		<div id="navigation">
-			<nav id="top-nav">
-				<ul id="top-nav-ul">
-					<li>Blog</li>
-					<li>Nourish</li>
-					<li>Shop</li>
-				</ul>
-			</nav>
+export default class Navigation extends React.Component {
+	constructor(props) {
+		super(props);
 
-			<nav id="sub-nav">
-				<div id="logo">
-					<img src={iFitLogo} alt="iFit Logo" />
-				</div>
-				<div id="sub-nav-links">
-					<ul id="sub-nav-ul">
-						<li>Exercise</li>
-						<li>Nutrition</li>
-						<li>Activity</li>
-						<li>Sleep</li>
+		this.state = {
+			prevScrollPosition: window.pageYOffset,
+			visible: true,
+		};
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll);
+	}
+
+	// method to hide/show navbar on scroll down/up
+	handleScroll = () => {
+		const { prevScrollPosition } = this.state;
+
+		const currentScrollPosition = window.pageYOffset;
+		const visible = prevScrollPosition > currentScrollPosition;
+
+		if (currentScrollPosition > 750) {
+			this.setState({
+				prevScrollPosition: currentScrollPosition,
+				visible: false,
+			});
+		} else {
+			this.setState({
+				visible: true,
+			});
+		}
+	};
+	render() {
+		return (
+			<div
+				className={classnames('navigation', {
+					'navigation--hidden': !this.state.visible,
+				})}
+			>
+				<nav id="top-nav">
+					<ul id="top-nav-ul">
+						<li>Blog</li>
+						<li>Nourish</li>
+						<li>Shop</li>
 					</ul>
-				</div>
-				<div id="nav-button">
-					<button>Sign Up</button>
-				</div>
-			</nav>
-		</div>
-	);
-}
+				</nav>
 
-export default Navigation;
+				<nav id="sub-nav">
+					<div id="logo">
+						<img src={iFitLogo} alt="iFit Logo" />
+					</div>
+					<div id="sub-nav-links">
+						<ul id="sub-nav-ul">
+							<li>Exercise</li>
+							<li>Nutrition</li>
+							<li>Activity</li>
+							<li>Sleep</li>
+						</ul>
+					</div>
+					<div id="nav-button">
+						<button>Sign Up</button>
+					</div>
+				</nav>
+			</div>
+		);
+	}
+}
